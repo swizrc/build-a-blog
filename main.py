dd1bf9ef2906f48b04210288bd776091c3cc3ddf
Sam <swizrc@mst.edu>
2017-10-19 19:23:13 -0500

    Login and Register Functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template,session
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,6 +29,83 @@ class User(db.Model):
 		self.name = name
 		self.password = password
 
+@app.before_request
+def require_login():
+	allowed_routes = ['register','login','blog','logout','index']
+	if 'user' not in session and request.endpoint not in allowed_routes:
+		return redirect('/login')
+		
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		usererror = ""
+		pwderror = ""
+		Error = False
+		username = request.form['username']
+		password = request.form['password']
+		user = User.query.filter_by(name=username).first()
+		if not user:
+			usererror = "nonexist"
+			Error = True
+		if user and user.password != password:
+			pwderror = "mismatch"
+			Error = True
+		if user and user.password == password and Error == False:
+			session['user'] = username
+			return redirect('/')
+		else:
+			return render_template('login.html',usererror=usererror,pwderror=pwderror)
+	else:
+		return render_template('login.html')
+		
+@app.route('/register', methods=['GET','POST'])
+def register():
+	if request.method == 'POST':
+		usererror = ""
+		pwderror = ""
+		vpwderror = ""
+		Error = False
+		username = request.form['username']
+		password = request.form['password']
+		verify = request.form['verify']
+		if User.query.filter_by(name=username).first()
+			usererror = "existing"
+			Error = True
+		if password != verify:
+			pwderror = "mismatch"
+			vpwderror = "mismatch"
+			Error = True
+		if len(username) < 3:
+			usererror = "short"
+			Error = True
+		if len(password) < 3:
+			pwderror = "short"
+			Error = True
+		if len(username) == 0:
+			usererror = "empty"
+			Error = True
+		if len(password) == 0:
+			pwderror = "empty"
+			Error = True
+		if len(verify) == 0:
+			vpwderror = "empty"
+			Error = True
+		if Error == False:
+			user = User(username,password)
+			db.session.add(user)
+			db.session.commit()
+			session['user'] = username
+			return redirect('/new_post')
+		else:
+			return render_template('register.html',usererror=usererror,pwderror=pwderror,vpwderror=vpwderror)
+	else:
+		return render_template('register.html')
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+	del session['user']
+	return redirect('/blog')
+
 @app.route('/')
 def index():
 	return redirect('/blog')
@@ -40,6 +117,7 @@ def blog():
 		return render_template('blog.html',post=BlogPost.query.get(id),blogid=id)
 	else:
 		return render_template('blog.html',posts=BlogPost.query.all())
+		
 @app.route('/new_post', methods=['GET','POST'])
 def new_post():
 	if request.method == 'POST':
